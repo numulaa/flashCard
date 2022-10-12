b53f545c0368601c5277b1e073797d6376ebcde8
Nurul Mukhlisa <numulaa@gmail.com>
2022-10-12 21:21:04 +0700

    added auth

--- routes/flashCard.js
+++ routes/flashCard.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const { ensureAuth } = require("../middleware/auth");
 const flashCardController = require("../controllers/flashCard");
 
 router.get("/", flashCardController.getFlashCards);
 router.post("/createFlashCard", flashCardController.createFlashCard);
-
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const passport = require("passport");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const flash = require("express-flash");
+const logger = require("morgan");
 const connectDB = require("./config/database");
 const homeRoutes = require("./routes/home");
 const flashCardRoutes = require("./routes/flashCard");
 
 require("dotenv").config({ path: "./config/.env" });
+require("./config/passport")(passport);
 
 connectDB();
 
@@ -12,10 +19,26 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(logger("dev"));
+
+//session
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  })
+);
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(flash());
 
 app.use("/", homeRoutes);
 app.use("/flashCard", flashCardRoutes);
 
 app.listen(process.env.PORT, () => {
-  console.log(`Running on port`);
+  console.log(`Server is running`);
 });
